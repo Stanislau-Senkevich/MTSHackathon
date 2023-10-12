8b8d2e7d05958ad4e80a62c5c5adccf7c6268ee6
Stanislau <it.senkevich@gmail.com>
2023-10-12 17:19:57 +0300

    try to fix

--- internal/repository/mongo/category.go
+++ internal/repository/mongo/category.go
@@ -4,12 +4,17 @@ import (
 	"MTSHackathonBackEnd/internal/config"
 	"MTSHackathonBackEnd/internal/entity"
 	"context"
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (m *MongoRepository) GetAllCategories() ([]entity.Category, error) {
+	fmt.Println(m.Config)
+	fmt.Println(m.DB)
 	coll := m.DB.Database(m.Config.DBName).Collection(
 		m.Config.Collections[config.CategoryCollection])
+
+	fmt.Println(coll)
 
 	cur, err := coll.Find(context.TODO(), bson.M{})
 	if err != nil {
